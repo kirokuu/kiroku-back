a71c393f7f83625b2799f053837ffcebe0e2e355
전병일 <jeonbye123@gmail.com>
2025-07-18 22:18:22 +0900

    swagger연동

--- src/main/java/com/example/kiroku/KirokuApplication.java
+++ src/main/java/com/example/kiroku/KirokuApplication.java
@@ -1,8 +1,15 @@
 package com.example.kiroku;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(title = "Kiroku API Docs",
+				version = "v1",
+				description = "Kiroku 서비스 API 명세서")
+)
 @SpringBootApplication
 public class KirokuApplication {
 
